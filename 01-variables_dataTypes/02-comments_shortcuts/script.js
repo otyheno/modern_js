4259cbc224d7fa05975e28f16ed56cd40295d330
otyheno <henryotieno66@gmail.com>
2023-04-14 10:37:43 +0300

    01-02-Js comments and Vs code Keyboard shortcuts

---
+++ 01-variables_dataTypes/02-comments_shortcuts/script.js
@@ -0,0 +1,42 @@
+// Single line comment - Ctrl+/ or Command+/
+
+console.log("Argument");
+
+console.log(47);
+
+console.log(20, "Arguments");
+
+/* Multi line comment - 
+console.error("Alert");
+console.warn("Warning");
+*/
+
+console.table({name: "Oty", mail:"henryotieno66@gmail.com"});
+
+console.group('Group Name')
+console.log(20, "Arguments");
+console.error("Alert");
+console.warn("Warning");
+console.groupEnd();
+
+/*Navigate code
+Ctrl + Arrow
+
+Highlight characters
+Ctrl + D
+Ctrl + shift + L
+Alt + Click
+*/
+
+
+/*Search file
+Ctrl + shift + O
+Ctrl + F
+*/
+
+/*Toggle Vscode Sidebar
+Ctrl + B
+
+Toggle Vscode Terminal
+Ctrl + ~
+*/
