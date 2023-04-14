b652f74c384e29b92c6ffec667b0e318e5935f3c
otyheno <henryotieno66@gmail.com>
2023-04-14 20:53:26 +0300

    01-04-data_types

---
+++ 01-variables_dataTypes/04-data_types/script.js
@@ -0,0 +1,30 @@
+// Type of
+
+//String
+const businessName = "KOTY";
+const output = businessName;
+console.log(output, typeof output);
+
+//Number
+const employees = 29;
+const output1 = employees;
+console.log(output1, typeof output1);
+
+const revenue= 6000.89;
+const output2 = revenue;
+console.log(output2, typeof output2);
+
+//Boolean
+const learningJS = true;
+const output3 = learningJS;
+console.log(output3, typeof output3);
+
+//Undefined
+let score;
+const output4 = score;
+console.log(output4, typeof output4);
+
+//Undefined
+const num = 8744000086974738n;
+const output5 = num;
+console.log(output5, typeof output5);
