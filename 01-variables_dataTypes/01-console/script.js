2d0b242a3b8ae47586f823831245a82bae7d2538
otyheno <henryotieno66@gmail.com>
2023-04-14 10:12:50 +0300

    first commit

---
+++ 01-variables_dataTypes/01-console/script.js
@@ -0,0 +1,19 @@
+//loging to the console using the console object
+
+console.log("Argument");
+
+console.log(47);
+
+console.log(20, "Arguments");
+
+console.error("Alert");
+
+console.warn("Warning");
+
+console.table({name: "Oty", mail:"henryotieno66@gmail.com"});
+
+console.group('Group Name')
+console.log(20, "Arguments");
+console.error("Alert");
+console.warn("Warning");
+console.groupEnd();
---
+++ modern_js.md
@@ -0,0 +1,11 @@
+
+What JS can be used for
+
+DOM( Document Object Model) Manipulation
+Event Handling
+Asynchronous Request
+Animations amd Effects
+Data manipulation (Sorting, filtering, etc)
+Storing data (Cookies, LocalStorage)
+Single page application
+Creating APIs and web services
