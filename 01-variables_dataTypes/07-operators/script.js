da516df341ff574968696dc18691728832eac896
otyheno <henryotieno66@gmail.com>
2023-04-16 21:18:26 +0300

    01-07-operators in js

---
+++ 01-variables_dataTypes/07-operators/script.js
@@ -0,0 +1,56 @@
+/*
+Arithmetic
+*/
+
+//addition
+y = 6+7;
+console.log(y);
+//subtraction
+y = 8-2;
+console.log(y);
+//multiplication
+y = 8*2;
+console.log(y);
+//division
+y = 8/2;
+console.log(y);
+//remainder/modulus
+y = 8%3;
+console.log(y);
+
+
+/*
+Concatenation (+ sign)
+*/
+
+let name = "Oty" + " " + "Heno";
+console.log(name);
+
+/*
+Exponent (**)
+*/
+x=8**2;
+console.log(x);
+
+/*
+Increment (++)
+*/
+x=8;
+x++;
+console.log(x);
+
+/*
+Decrement (--)
+*/
+x=8;
+x--;
+console.log(x);
+
+/*
+Assignment (=)
+*/
+x=3;
+
+/*
+Comparison (== < > != <= >= ===)
+*/
